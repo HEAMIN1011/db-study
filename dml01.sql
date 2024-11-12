12b0a8df57bb985613f59b4d1f08e0f16e9aa8ab
HEAMIN1011 <mintwo20@nate.com>
2024-11-12 15:08:49 +0900

    외래키설정, INSERT 여러개, MERGE

--- dml01.sql
+++ dml01.sql
@@ -1,80 +1,96 @@
 --dml
 
---데이터 저장
---데이터 수정
---데이터 삭제
---데이터 병합
+--데이터 저장 INSERT
+--데이터 수정 UPDATE
+--데이터 삭제 DELETE
+--데이터 병합 MERGE
 
-insert into 테이블명 (컬럼명)
-values (데이터 값);
+select * from new_table;
 
-insert into new_table (no, name, birth)
-values (1, '이름1', sysdate);
+INSERT INTO 테이블명 (컬럼명...)
+VALUES (데이터 값...);
 
-insert into new_table (name, birth, no)
-values ('이름2', sysdate, 2);
+INSERT INTO new_table (no, name, birth)
+VALUES (1, '이름1', SYSDATE);
 
-insert into new_table (name, birth, no)
-values ('이름3','2002-02-15', 3);
+INSERT INTO new_table (name, birth, no)
+VALUES ('이름2', SYSDATE, 2);
 
-insert into new_table (name, birth, no)
-values ('이름4', to_date('2001-10-25'), 4);
+INSERT INTO new_table (name, birth, no)
+VALUES ('이름3', '2002-02-15', 3);
+
+INSERT INTO new_table (name, birth, no)
+VALUES ('이름4', TO_DATE('2001-10-25'), 4);
 
 --컬럼명 생략
 --전체 컬럼에 저장하는 경우 + 순서
-insert into new_table
-values (5, '이름5', sysdate);
+INSERT INTO new_table 
+VALUES (5, '이름5', SYSDATE);
 
-insert into new_table (no, name)
-values (6, '이름6');
+INSERT INTO new_table (no, name)
+VALUES (6, '이름6');
 
-insert into new_table
-values (7, '이름7', null);
+INSERT INTO new_table 
+VALUES (7, '이름7', null);
+
+---여러개를 동시에 삽입(저장) INSERT ALL
+select * from tt02;
+
+INSERT ALL
+INTO tt02 VALUES (7, '이름7', null)
+INTO tt02 VALUES (8, '이름7', null)
+INTO tt02 VALUES (9, '이름7', null)
+INTO tt02 VALUES (10, '이름7', null)
+select * from dual;
+
+INSERT INTO tt02    --number3 varchar2 date
+SELECT 11, '샘플', SYSDATE FROM dual;
+
+INSERT INTO tt02
+select deptno, loc, SYSDATE  from dept;
 
 
----
+INSERT INTO tt02 (no)
+VALUES (1);
 
-insert into tt02 (no)
-values (1);
-
-insert into tt02 (no, name, hiredate)
-values (2, null, null);
+INSERT INTO tt02 (no, name, hiredate)
+VALUES (2, null, null);
 
 
---update 수정
 
-update 테이블명
-set 컬럼명 = 값
-where 조건;
+--Update 수정
 
---임시부서 여부 체크 컬럼명 temp_code 'Y' : 임시부서, 'N': 정식부서
+UPDATE 테이블명
+SET 컬럼명 = 값
+WHERE 조건;
+
+--임시부서 여부를 체크   컬럼명 temp_code   'Y' 임시부서 , 'N' 정식부서
 select * from dept4;
 
-update dept4
-set temp_code = 'N';
+UPDATE dept4
+SET temp_code = 'N';
 
-update dept4
-set temp_code = 'Y'
-where dcode = 2000;
+UPDATE dept4
+SET temp_code = 'Y'
+WHERE dcode = 2000;
 
 select *
 from dept4
-where temp_cpde = 'Y';
+where temp_code = 'Y';
 
 
-create table professor2
-as
-select * from professor;
+CREATE TABLE professor2
+AS
+SELECT * FROM professor;
 
 select *
 from professor2
 where position = 'assistant professor';
 
-update professor2
---set bonus = 200   --200으로 저장
-set bonus = bonus + 200 --200을 인상
+UPDATE professor2
+--SET bonus = 200  --200으로 저장
+SET bonus = bonus + 200  --200을 인상
 where position = 'assistant professor';
-
 
 commit;
 rollback;
